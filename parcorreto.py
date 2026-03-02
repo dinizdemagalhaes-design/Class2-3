8bba49e12a4ec3693193d1e7bf23bddde6b1a434
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-02 11:50:55 -0300

    Add parity check for Ewerton and Rodrigo's numbers
    Implement input and parity check for two numbers.

---
+++ parcorreto.py
@@ -0,0 +1,14 @@
+Ewerton = int(input("Digite o número de Ewerton")) 
+Rodrigo = int(input("Digite onúmero de Rodrigo"))
+
+
+while Ewerton or Rodrigo != 0:
+    Ewerton = int(input("Digite o número de Ewerton:"))
+    Rodrigo = int(input("Digite onúmero de Rodrigo:"))
+    if  int(Ewerton)%2==0:
+        print("Esse número é par")
+        
+    else: 
+        print("O número é ímpar")
+
+print("O maior número par ou ímpar é:", )
