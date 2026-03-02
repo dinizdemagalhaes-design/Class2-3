78b5add338b163ea29cd79da5dc8e7da99382040
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-02 11:26:14 -0300

    Implement triangle type determination logic

---
+++ triangulofinal.py
@@ -0,0 +1,22 @@
+
+
+while lado1 +lado2 >= lado3: 
+    lado1 = float(input("Digite o primeiro lado"))
+    lado2 = float(input("Digite o segundo lado"))
+    lado3 = float(input("Digite o treceiro lado"))
+    print("Forma Triângulo")
+    if lado1==lado2==lado3:
+        print("Equilátero")
+    if lado1==lado2 or lado1==lado3 or lado2==lado3:
+        print("Isósceles")
+    elif lado1!=lado2!=lado3:
+        print("Escaleno")
+    else: 
+        print("Não forma triângulo")
+    lado1 = float(input("Digite o primeiro lado"))
+    lado2 = float(input("Digite o segundo lado"))
+    lado3 = float(input("Digite o treceiro lado"))
+
+
+
+Print("Forma Triângulo:;" if, else, elif, )
