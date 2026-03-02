b9c3546a0518e5204256443474b1160c7aa5bc34
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-02 11:27:07 -0300

    Correct parouimpar.py logic and syntax
    Fix logic and syntax errors in parouimpar.py.

---
+++ parouimpar.py
@@ -0,0 +1,13 @@
+Ewerton = par 
+Rodrigo = ímpar
+
+
+while número ==0:
+    if  par:
+        print(O resultado é para (divisível por 2))
+        
+    else ímpar:
+        print(O resultado é ímpar ( não é divisível por 2 ))
+
+print("O maior número par ou ímpar é:", while)
+     
